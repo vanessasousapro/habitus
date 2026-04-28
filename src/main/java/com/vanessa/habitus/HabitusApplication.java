ca4d83f903daad0985c29d88a2e50db6eac15bf4
Vanessa Amancio <vanessasousa.pro@gmail.com>
2026-04-28 17:33:17 -0300

    chore: initial project setup with Spring Initializr

---
+++ src/main/java/com/vanessa/habitus/HabitusApplication.java
@@ -0,0 +1,13 @@
+package com.vanessa.habitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanessa/habitus/HabitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.habitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
